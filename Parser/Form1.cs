f799f04c7f598cbacfad8db1110298f0d23a9f55
Utkir.Turakulov <utkir.turakulov@simbirsoft.com>
2019-01-17 11:16:17 +0400

    Рефакторинг структуры проекта

--- Parser/Form1.cs
+++ Parser/Form1.cs
@@ -14,6 +14,11 @@ namespace Parser
             InitializeComponent();
         }
 
+		/// <summary>
+		/// Кнопка parse
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             FileReader reader = new FileReader();
@@ -22,54 +27,11 @@ namespace Parser
             string page = reader.GetLines(path);
 
             var document = new HtmlParser().ParseDocument(page);
-
             Catalog catalog = new Catalog(document);
+			Parser parser = new Parser();
 
-            var elements = catalog.GetElements();
-
-            DataTable dataTable = GetDataTable(elements);
-
-            dataGridView1.DataSource = dataTable;
+            dataGridView1.DataSource = parser.GetDataTable(catalog.GetElements());
         }
 
-
-        public DataTable GetDataTable(List<FieldSet> data)
-        {
-            DataTable table = new DataTable();
-
-            DataColumn user = new DataColumn("Пользователь", Type.GetType("System.String"));
-            DataColumn date = new DataColumn("Дата", Type.GetType("System.String"));
-            DataColumn quantity = new DataColumn("Количество", Type.GetType("System.String"));
-            DataColumn real = new DataColumn("Реальная", Type.GetType("System.String"));
-            DataColumn income = new DataColumn("Входная", Type.GetType("System.String"));
-            DataColumn retail = new DataColumn("Розница", Type.GetType("System.String"));
-            DataColumn corp = new DataColumn("Корп.", Type.GetType("System.String"));
-            DataColumn provider = new DataColumn("Поставщик", Type.GetType("System.String"));
-
-            table.Columns.Add(user);
-            table.Columns.Add(date);
-            table.Columns.Add(quantity);
-            table.Columns.Add(real);
-            table.Columns.Add(income);
-            table.Columns.Add(retail);
-            table.Columns.Add(corp);
-            table.Columns.Add(provider);
-
-            foreach (var i in data)
-            {
-                table.Rows.Add(new object[] {
-                    i.User,
-                    i.Date,
-                    i.Quantity,
-                    i.Real,
-                    i.Income,
-                    i.Retail,
-                    i.Corp,
-                    i.Provider
-                });
-            }
-
-            return table;
-        }
     }
 }
---
+++ Parser/Parser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Parser
+{
+	public class Parser
+	{
+		/// <summary>
+		/// Преобразует входной набор данных в DataTable
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public DataTable GetDataTable(List<FieldSet> data)
+		{
+			DataTable table = new DataTable();
+
+			DataColumn user = new DataColumn("Пользователь", Type.GetType("System.String"));
+			DataColumn date = new DataColumn("Дата", Type.GetType("System.String"));
+			DataColumn quantity = new DataColumn("Количество", Type.GetType("System.String"));
+			DataColumn real = new DataColumn("Реальная", Type.GetType("System.String"));
+			DataColumn income = new DataColumn("Входная", Type.GetType("System.String"));
+			DataColumn retail = new DataColumn("Розница", Type.GetType("System.String"));
+			DataColumn corp = new DataColumn("Корп.", Type.GetType("System.String"));
+			DataColumn provider = new DataColumn("Поставщик", Type.GetType("System.String"));
+
+			table.Columns.Add(user);
+			table.Columns.Add(date);
+			table.Columns.Add(quantity);
+			table.Columns.Add(real);
+			table.Columns.Add(income);
+			table.Columns.Add(retail);
+			table.Columns.Add(corp);
+			table.Columns.Add(provider);
+
+			foreach (var i in data)
+			{
+				table.Rows.Add(new object[] {
+					i.User,
+					i.Date,
+					i.Quantity,
+					i.Real,
+					i.Income,
+					i.Retail,
+					i.Corp,
+					i.Provider
+				});
+			}
+
+			return table;
+		}
+	}
+}
